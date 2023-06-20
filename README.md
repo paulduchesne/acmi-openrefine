6d08023c101e47b4c6752a270985fbbf458d10c4
paulduchesne <paulduchesne@tuta.io>
2023-06-20 22:25:57 +1000

    added extant ids

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # ACMI OpenRefine
 
 Preprocessing scripts and resources for using ACMI API data with Wikidata via OpenRefine.
+
+## Run Preprocessor
+
+The `./run.sh` script will ensure that API data is pulled and current, and generate CSVs for use in OpenRefine.
+
+## Install OpenRefine
+
+Installation options for OpenRefine can be found [here](https://openrefine.org/download). Version 3.7 has been used in all testing so far.
+
+On Linux the downloaded tar.gz file can be unpackaged and then launched by running `./refine` from within the unpacked directory. This will result in a service running at http://127.0.0.1:3333/.
+
+## Reconciling Data
+
+To begin working with OpenRefine, select one of the CSVs as data source. For this example we are using `acmi_film.csv`. Select `Next`.
+
+Rename the project as desired (under `Project name`) and then select `Create project`.
+
+Select arrow next to `work_label` and then `Reconcile` -> `Start reconciling...`
+
+The only service which will likely be visible will be `Wikidata reconci.link (en)`. Select this.
+
+The reconciliation service should automaticatly detect that each cell should be against the type of `film (Q11424)`, otherwise you may need to specify this under `Reconcile against type`.
+
+Add `director_label` by selecting that checkbox on the right hand side, and `As property` enter `P57`.
+
+Select `Start reconciling...`
+
+This should result in a table where entries are highlighted in dark blue if they have been succesfully matched with Wikidata items. It is possible to hover over these to confirm that the description matches what is expected. Entities which have been matched in the past will automatically be picked up.
+
+Other entires will need disambiguation as OpenRefine cannot determine a match on its own. Here you may select the proper candidate from the list by using the checkbox icon on the left of each option. 
+
+It is quite possible that some entires do not exist in Wikidata and will need to be created. If this is the case, select `Create new item` at the bottom of the options.
+
--- python/acmi_film.py
+++ python/acmi_film.py
@@ -7,12 +7,24 @@ import pathlib
 import pydash
 import tqdm
 
+def replacer(row, flavour, source):
+
+    ''' Function to replace "labels" with Wikidata codes where they already exist against ACMI IDs. '''
+
+    if row[f'{flavour}_id'] in source.keys():
+        return source[row[f'{flavour}_id']]
+    else:
+        return row[f'{flavour}_label']
+
 film_dataframe = pandas.DataFrame(columns=['work_id', 'work_label', 'director_id', 'director_label'])
+
+work_wikidata = dict()
+director_wikidata = dict()
 
 api_path = pathlib.Path.cwd() / 'acmi-api' / 'app' / 'json' / 'works'
 api_files = [filename for filename in api_path.iterdir() if filename.suffix == '.json']
 
-for api_file in tqdm.tqdm(api_files[:10]):
+for api_file in tqdm.tqdm(api_files):
     with open(api_file, encoding='utf-8') as api_data:
         api_data = json.load(api_data)
 
@@ -22,8 +34,26 @@ for api_file in tqdm.tqdm(api_files[:10]):
             if 'creators_primary' in api_data:
                 for direct in [x for x in api_data['creators_primary'] if x['role'] == 'director']:
                     film_dataframe.loc[len(film_dataframe)] = [(api_data['id']), (work_label), (direct['creator_id']), (direct['name'])]
+                    if 'creator_wikidata_id' in direct:
+                        if direct['creator_wikidata_id']:
+                            director_wikidata[(direct['creator_id'])] = direct['creator_wikidata_id']
+
+            wikidata_id = ''
+            if 'external_references' in api_data:
+                for external_reference in api_data['external_references']:
+                    if pydash.get(external_reference, 'source.name') == 'Wikidata':
+                        wikidata_id = external_reference['source_identifier']
+
+            if wikidata_id != '':
+                work_wikidata[(api_data['id'])] = wikidata_id
+
+film_dataframe['work_label'] = film_dataframe.apply(replacer, flavour='work', source=work_wikidata, axis=1)
+film_dataframe['director_label'] = film_dataframe.apply(replacer, flavour='director', source=director_wikidata, axis=1)
 
 dataframe_path = pathlib.Path.cwd() / 'csv' / 'acmi_film.csv'
 dataframe_path.parents[0].mkdir(exist_ok=True)
 
 film_dataframe.to_csv(dataframe_path, index=False)
+
+print(len(film_dataframe))
+print(film_dataframe.head())
